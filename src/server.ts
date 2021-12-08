cb03ac0c01644d98bbb318ec513aa8990ccf940f
roman-oleynik <raman.alejnik@gmail.com>
2021-12-08 21:55:28 +0300

    init: add typescript

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+const { PORT } = require('./common/config');
+const httpServer = require('./app');
+
+httpServer.listen(PORT, () =>
+  console.log(`App is running on http://localhost:${PORT}`)
+);
+
